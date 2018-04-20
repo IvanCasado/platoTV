84f7ffd49723133d5f7bdb75eb2794fd8aa3145f
homago <manu.gnz@gmail.com>
2018-04-20 19:53:03 +0200

    Primer commit

--- src/platotv/PlatoTv.java
+++ src/platotv/PlatoTv.java
@@ -16,6 +16,8 @@ public class PlatoTv {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Manuel");
     }
     
 }
